e77aff5d3d88f356d556ad8d561671758afae861
ehteshamkhan900 <mehteshamkhan900@gmail.com>
2023-09-27 22:30:28 +0530

    Initialized Frontend

---
+++ script.js
@@ -0,0 +1,18 @@
+<span class="form-item-icon material-symbols- rounded">lock</span>
+<span class="form-item-icon material-symbols- rounded">mail</span>
+
+
+<div class="login-card-social">
+<div>Other Sign-in Platform</div> <div class="login-card-social-btn">
+<a href="#">
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler
+icon-tabler-brand-facebook" width="44" height="44" viewBox="0 0 24 24" stroke- width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke- linejoin="round">
+<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+<path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v- 4h-3a5 5 0 0 0 -5 5v2h-3" />
+</svg> </a>
+<a href="#">
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler
+icon-tabler-brand-google" width="44" height="44" viewBox="0 0 24 24" stroke- width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke- linejoin="round">
+<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+<path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8" /> </svg>
+</a> </div>
